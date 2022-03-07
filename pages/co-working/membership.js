9f838f659ad55b2e44883d362b67b1a2989d90fc
raihan <rishaon0@gmail.com>
2022-03-07 17:29:28 +0600

    Faq Added

--- pages/co-working/membership.js
+++ pages/co-working/membership.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-const membership = () => {
-    return (
-        <div>
-            <h1>hello members</h1>
-            <Link href="/">back</Link>
-        </div>
-    );
+const Membership = () => {
+  return (
+    <div>
+      <h1>hello members</h1>
+      <Link href="/">back</Link>
+    </div>
+  );
 };
 
-export default membership;
+export default Membership;
