bb75b562c58d3da1f6d7e19bd534fce319219740
raihan-bin-islam <rishaon0@gmail.com>
2022-03-15 02:38:49 +0600

    about us purpose section added

--- components/About_Us/AboutUsPurpose/AboutUsPurpose.js
+++ components/About_Us/AboutUsPurpose/AboutUsPurpose.js
@@ -1,20 +1,38 @@
 import React from "react";
+import BackgroundOverlay from "../../Shared/BackgroundOverlay/BackgroundOverlay";
+import Button from "../../Shared/Button/Button";
 import AboutUsDescription from "../AboutUsDescription/AboutUsDescription";
 import { purpose } from "./description";
+
 const AboutUsPurpose = () => {
   return (
     <section className="about_us_purpose">
+      <BackgroundOverlay className={"about_us_purpose_bg_overlay"} />
       <div className="about_us_purpose_container">
         <div className="about_us_purpose_container_detail">
           <AboutUsDescription
             parentClass={"about_us_purpose_container_detail"}
             heading={purpose.heading}
           />
-          <p className="about_us_purpose_container_detail_highlight">{purpose.highlight}</p>
+          <div className="about_us_purpose_container_detail_highlight">
+            <p>{purpose.highlight}</p>
+          </div>
+
           <AboutUsDescription
             parentClass={"about_us_purpose_container_detail"}
             content={purpose.content}
           />
+        </div>
+        <div className="about_us_purpose_container_btn_grid">
+          <Button text={"We Inspire"} />
+          <Button text={"We Connect"} />
+          <Button text={"We Enable"} />
+          <Button text={"We Inspire"} />
+          <Button text={"We Connect"} />
+          <Button text={"We Enable"} />
+          <Button text={"We Inspire"} />
+          <Button text={"We Connect"} />
+          <Button text={"We Enable"} />
         </div>
       </div>
     </section>
---
+++ components/Shared/BackgroundOverlay/BackgroundOverlay.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BackgroundOverlay = ({ className }) => {
+  return (
+    <div className={className}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1079">
+        <path
+          id="Intersection_4"
+          data-name="Intersection 4"
+          d="M379.731,1083.5c-262.775-7.628-438-69.105-552.229-159.163V4.5h1920V695.657c-250.176,185.692-616.547,345.707-1148.629,382.736-35.981,2.5-70.829,4.2-104.3,5.107Z"
+          transform="translate(172.498 -4.5)"
+          fill="#f2f2f2"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default BackgroundOverlay;
