a76a79bfbd9e613b2ba5057bbd02c5527b3b1b61
raihan <rishaon0@gmail.com>
2022-03-16 18:33:47 +0600

    minor style fix

--- components/Shared/TitleBlock/TitleBlock.js
+++ components/Shared/TitleBlock/TitleBlock.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const TitleBlock = ({ title, shape, parentClass }) => {
-    return (
-        <>
-            <div className={`${parentClass}_title_block_container`}>
-                <img src={shape} />
-                <h1>{title}</h1>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={`${parentClass}_title_block_container`}>
+        <img src={shape} />
+        <h1>{title}</h1>
+      </div>
+    </>
+  );
 };
 
 export default TitleBlock;
