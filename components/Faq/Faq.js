f16569887b40f7c8e8248f6ec443fb4ce7df7474
ShantoKumarSarker <sk.bd2007@gmail.com>
2022-03-16 18:22:14 +0600

    faq base component and page added

--- components/Faq/Faq.js
+++ components/Faq/Faq.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import commonShape from '../../public/common_path.png';
 
-const Careers = () => {
+const Faq = () => {
     return (
-        <section className='careers_container'>
-            <div className='careers_container_content'>
-                <div className='careers_container_content_left'>
+        <section className='faq_container'>
+            <div className='faq_container_content'>
+                <div className='faq_container_content_left'>
+                    <h1>
+                        <span>Questions you may come across before</span>
+                        starting your journey:
+                    </h1>
                     <p>Impact Hub Dhaka founders bring in more than 35 years experience of building social businesses, technology companies and social impact projects in Bangladesh.</p>
                     <p>We’re looking for community managers, business developers and community hosts to join our growing team! Get in touch at dhaka@impacthub.net.</p>
                 </div>
-                <div className='careers_container_content_right'>
-                    <h1>Excited to create impact together?</h1>
+                <div className='faq_container_content_right'>
+                    <h1>Come create with us!</h1>
                     <img src={commonShape.src}/>
                 </div>
             </div>
@@ -18,4 +22,4 @@ const Careers = () => {
     );
 };
 
-export default Careers;
+export default Faq;
