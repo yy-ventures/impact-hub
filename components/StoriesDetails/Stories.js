e8361a08f348562d6d484bf737940b1fb0bd7eb4
Khandaker Nabiul <na.ninad60@gmail.com>
2022-03-29 14:43:23 +0600

    stories details page folder created

---
+++ components/StoriesDetails/Stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// LAYOUT
+import layout from './layout/StoriesSvg'
+
+import './stories.scss'
+export default function Stories() {
+  return (
+    <div>
+        <h1>stories</h1>
+    </div>
+  )
+}
