71f8ea455f2d5b8989d7156bb006850a4be82fcb
Khandaker Nabiul <na.ninad60@gmail.com>
2022-03-31 18:08:29 +0600

    newletter section button component added

--- components/Home/HomeNewsLetter/HomeNewsLetter.js
+++ components/Home/HomeNewsLetter/HomeNewsLetter.js
@@ -33,6 +33,7 @@ const HomeNewsLetter = () => {
   };
   return (
     <section className="home_news_letter">
+        <div className="home_impacts_layout"><img src={layout2.src}/></div>
       <div className="home_news_letter_left">
         {showForm && (
           <div>
