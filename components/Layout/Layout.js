f764363c7df319d7e965c8b2240b707521a70cfb
Mamun <mmamun.roshid@gmail.com>
2022-04-03 14:26:02 +0600

    add custom fonts

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import React from "react";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
@@ -6,39 +5,6 @@ import Navbar from "../Shared/Navbar/Navbar";
 const Layout = ({ children }) => {
   return (
     <div className="layout">
-      <Head>
-        <link
-          href="../../public/fonts/GTWalsheimPro/GTWalsheimPro-Bold.woff"
-          rel="stylesheet"
-        />    <link
-          href="../../public/fonts/GTWalsheimPro/GTWalsheimPro-Bold.woff2"
-          rel="stylesheet"
-        />
-        <link
-          href="../../public/fonts/GTWalsheimPro/GTWalsheimPro-Light.woff"
-          rel="stylesheet"
-        />
-        <link
-          href="../../public/fonts/GTWalsheimPro/GTWalsheimPro-Light.woff2"
-          rel="stylesheet"
-        />
-        <link
-          href="../../public/fonts/GTWalsheimPro/GTWalsheimPro-Medium.woff"
-          rel="stylesheet"
-        />
-        <link
-          href="../../public/fonts/GTWalsheimPro/GTWalsheimPro-Medium.woff2"
-          rel="stylesheet"
-        />
-        <link
-          href="../../public/fonts/GTWalsheimPro/GTWalsheimPro-Regular.woff"
-          rel="stylesheet"
-        />
-        <link
-          href="../../public/fonts/GTWalsheimPro/GTWalsheimPro-Regular.woff2"
-          rel="stylesheet"
-        />
-      </Head>
       <Navbar />
       {children}
       <Footer />
