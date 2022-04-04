abcc0d765f5d3fd997b1fa4f9694fed8bcdc279a
raihan <rishaon0@gmail.com>
2022-04-04 16:12:40 +0600

    office space design fix, feedback design fix & past events added

--- components/About_Us/AboutUsApproach/AboutUsApproach.js
+++ components/About_Us/AboutUsApproach/AboutUsApproach.js
@@ -2,6 +2,7 @@ import React from "react";
 import banner from "../../../public/images/about-us/approach_banner.png";
 import CardDetail from "./CardDetail/CardDetail";
 import { cardDetails } from "./contents";
+import BannerSvg from "./BannerSvg/BannerSvg";
 const AboutUsApproach = () => {
   return (
     <section className="about_us_approach">
@@ -12,24 +13,41 @@ const AboutUsApproach = () => {
           </div>
           <div className="about_us_approach_container_inner_body">
             <p>
-              Embedded in Design Thinking, we foster collaboration, unlock creativity and create
-              innovative solutions:
+              Embedded in Design Thinking, we foster collaboration, unlock creativity and create innovative solutions:
             </p>
             <p>
               <strong>Inspire:</strong> challenging the status quo is possible – we do it every day.
             </p>
             <p>
-              <strong>Connect:</strong> accessing a professional ecosystem of change makers and
-              curating interaction across silos.
+              <strong>Connect:</strong> accessing a professional ecosystem of change makers and curating interaction
+              across silos.
             </p>
             <p>
               <strong>Enable:</strong>
-              we disrupt mindsets, share expertise and the necessary tools to create a better future
-              for all.
+              we disrupt mindsets, share expertise and the necessary tools to create a better future for all.
             </p>
           </div>
-          <div className="about_us_approach_container_inner_image">
-            <img src={banner.src} alt="" />
+          <div className="about_us_approach_container_inner_banner">
+            <div className="about_us_approach_container_inner_banner_svg">
+              <BannerSvg type="empathize" />
+              <p>Empathize</p>
+            </div>
+            <div className="about_us_approach_container_inner_banner_svg">
+              <BannerSvg type="define" />
+              <p>Define</p>
+            </div>
+            <div className="about_us_approach_container_inner_banner_svg">
+              <BannerSvg type="ideate" />
+              <p>Ideate</p>
+            </div>
+            <div className="about_us_approach_container_inner_banner_svg">
+              <BannerSvg type="prototype" />
+              <p>Prototype</p>
+            </div>
+            <div className="about_us_approach_container_inner_banner_svg">
+              <BannerSvg type="test" />
+              <p>Test</p>
+            </div>
           </div>
         </div>
         <div className="about_us_approach_container_grid">
--- components/Events/PastEvents/PastEvents.js
+++ components/Events/PastEvents/PastEvents.js
@@ -1,169 +1,128 @@
-import React from 'react';
+import React from "react";
 import Slider from "react-slick";
-import EventCommonCard from '../../Shared/EventCommoncard/EventCommonCard';
-import storyThumb from '../../../public/images/story_thumb.png'
-import HeadingDescription from '../../Shared/HeadingDescription/HeadingDescription';
-import PrevArrow from './Arrows/PrevArrow';
-import NextArrow from './Arrows/NextArrow';
-import storyPath from '../../../public/story_path.png';
-
+import EventCommonCard from "../../Shared/EventCommoncard/EventCommonCard";
+import storyThumb from "../../../public/images/story_thumb.png";
+import HeadingDescription from "../../Shared/HeadingDescription/HeadingDescription";
+import PrevArrow from "./Arrows/PrevArrow";
+import NextArrow from "./Arrows/NextArrow";
+import storyPath from "../../../public/story_path.png";
+import useFetch from "../../Hooks/useFetch";
 
 const PastEvents = () => {
+  // Fetch data of past events
+  const pastEvents = useFetch("/get-event?type=past");
+  const baseUrlForImages = process.env.baseUrlForImages;
 
-    const childSliderSettings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerMode: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: <PrevArrow />,
-        nextArrow: <NextArrow />,
-        responsive: [
-            {
-                breakpoint: 600,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    slidesToShow: 1
-                }
-            }
-        ]
-    };
+  //   Sort the events based on year and months
+  pastEvents.sort((a, b) => {
+    if (a.year === b.year) {
+      return a.month < b.month ? -1 : 1;
+    } else {
+      return a.year < b.year ? -1 : 1;
+    }
+  });
 
-    const parentSliderSettings = {
-        dots: false,
-        infinite: true,
-        speed: 1500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 600,
-                settings: {
-                    arrows: false,
-                    slidesToShow: 1
-                }
-            }
-        ]
-    };
-    
-    return (
-        <section className='past_events'>
-            {/* event path */}
-            <div className='past_events_path'>
-                <img src={storyPath.src} />
-            </div>
+  const childSliderSettings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-            {/* past event main */}
-            <div className='past_events_header'>
-                <HeadingDescription 
-                    parentClass="past_events_header"
-                    span='Past Impact Hub Dhaka' 
-                    heading='Events' />
-            </div>
-            <div className='past_events_container'>
-                <Slider {...parentSliderSettings}>
-                    {/* slider container */}
-                    <div className='past_events_container_data'>
-                        <div className='past_events_container_data_header'>
-                            <h3>November 2022</h3>
-                        </div>
-                        <div className='past_events_container_data_body'>
-                            <Slider {...childSliderSettings}>
-                                <EventCommonCard
-                                    title='Workshop on community building and the art of networking'
-                                    date='7 September 2020 - Dhaka'
-                                    time='3:00pm to 5:00 pm'
-                                    type='center'
-                                    thumb={storyThumb.src}
-                                />
-                                <EventCommonCard
-                                    title='Workshop on community building and the art of networking'
-                                    date='7 September 2020 - Dhaka'
-                                    time='3:00pm to 5:00 pm'
-                                    type='center'
-                                    thumb={storyThumb.src}
-                                />
-                                <EventCommonCard
-                                    title='Workshop on community building and the art of networking'
-                                    date='7 September 2020 - Dhaka'
-                                    time='3:00pm to 5:00 pm'
-                                    type='center'
-                                    thumb={storyThumb.src}
-                                />
-                                <EventCommonCard
-                                    title='Workshop on community building and the art of networking'
-                                    date='7 September 2020 - Dhaka'
-                                    time='3:00pm to 5:00 pm'
-                                    type='center'
-                                    thumb={storyThumb.src}
-                                />
-                            </Slider>
-                        </div>
-                    </div>
+  const parentSliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-                    {/* slider */}
-                    <div className='past_events_container_data'>
-                        <div className='past_events_container_data_header'>
-                            <h3>October 2022</h3>
-                        </div>
-                        <div className='past_events_container_data_body'>
-                            <Slider {...childSliderSettings}>
-                                <EventCommonCard
-                                    title='Workshop on community building and the art of networking'
-                                    date='7 September 2020 - Dhaka'
-                                    time='3:00pm to 5:00 pm'
-                                    type='center'
-                                    thumb={storyThumb.src}
-                                />
-                                <EventCommonCard
-                                    title='Workshop on community building and the art of networking'
-                                    date='7 September 2020 - Dhaka'
-                                    time='3:00pm to 5:00 pm'
-                                    type='center'
-                                    thumb={storyThumb.src}
-                                />
-                                <EventCommonCard
-                                    title='Workshop on community building and the art of networking'
-                                    date='7 September 2020 - Dhaka'
-                                    time='3:00pm to 5:00 pm'
-                                    type='center'
-                                    thumb={storyThumb.src}
-                                />
-                                <EventCommonCard
-                                    title='Workshop on community building and the art of networking'
-                                    date='7 September 2020 - Dhaka'
-                                    time='3:00pm to 5:00 pm'
-                                    type='center'
-                                    thumb={storyThumb.src}
-                                />
-                            </Slider>
-                        </div>
-                    </div>
-                </Slider>
-            </div>
+  return (
+    <section className="past_events">
+      {/* event path */}
+      <div className="past_events_path">
+        <img src={storyPath.src} />
+      </div>
 
+      {/* past event main */}
+      <div className="past_events_header">
+        <HeadingDescription parentClass="past_events_header" span="Past Impact Hub Dhaka" heading="Events" />
+      </div>
+      <div className="past_events_container">
+        <Slider {...parentSliderSettings}>
+          {pastEvents.length > 0 &&
+            pastEvents.map(({ date, events }, index) => {
+              return (
+                <div key={index} className="past_events_container_data">
+                  <div className="past_events_container_data_header">
+                    <h3>{date}</h3>
+                  </div>
+                  <div className="past_events_container_data_body">
+                    {(childSliderSettings.infinite = events.length > 3)}
+                    <Slider {...childSliderSettings}>
+                      {events.map(({ id, title, start_date, end_date, starts_at, ends_at, image_path }) => {
+                        return (
+                          <EventCommonCard
+                            key={id}
+                            title={title}
+                            date={`${start_date} - ${end_date}`}
+                            starts_at={starts_at}
+                            ends_at={ends_at}
+                            type="center"
+                            thumb={baseUrlForImages + image_path}
+                          />
+                        );
+                      })}
+                    </Slider>
+                  </div>
+                </div>
+              );
+            })}
+          {/* slider container */}
+        </Slider>
+      </div>
 
-
-            {/* // events footer */}
-            <div className='past_events_footer'>
-                <HeadingDescription
-                    span='Discover Our'
-                    heading='Space'
-                    paragraph='We are glad that you have found us! We would like to invite you and 
+      {/* // events footer */}
+      <div className="past_events_footer">
+        <HeadingDescription
+          span="Discover Our"
+          heading="Space"
+          paragraph="We are glad that you have found us! We would like to invite you and 
                 your team to visit and explore our space. If you are looking for an office 
                 for your startup, Impact Hub Dhaka is the right place for you. Book your 
-                spot for a trial day or week and check us out.'
-                />
-            </div>
-        </section>
-    );
+                spot for a trial day or week and check us out."
+        />
+      </div>
+    </section>
+  );
 };
 
 export default PastEvents;
