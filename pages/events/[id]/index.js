a1b8069025cf9873b703fd04f930e79e3f6a4659
farhan21 <farhanabsar21@gmail.com>
2022-04-05 13:48:01 +0600

    changes route id

---
+++ pages/events/[id]/index.js
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import useFetch from '../../../components/Hooks/useFetch';
+import StoryDetails from '../../../components/StoryDetails/StoryDetails';
+
+const storyDetails = () => {
+    let router = useRouter()
+    const { id } = router.query
+    const detailStory = useFetch(`/get-event-detail/${id}`)
+    return (
+        <>
+        {
+            detailStory[0] && <StoryDetails html={detailStory[0].details}></StoryDetails>
+        }
+        </>
+        
+    );
+};
+
+export default storyDetails;
--- pages/events/upcoming-events/index.js
+++ pages/events/upcoming-events/index.js
@@ -9,7 +9,7 @@ const Upcoming = () => {
                 <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
                 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
             </Head>
-           <PastEvents/>
+           <PastEvents type='upcoming'/>
         </>
     );
 };
