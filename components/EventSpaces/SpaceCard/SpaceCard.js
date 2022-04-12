16c1db02c9bcd41a0fe436e161475ef18e4527f2
raihan-bin-islam <rishaon0@gmail.com>
2022-04-13 03:50:17 +0600

    event space card responsive design fix

--- components/EventSpaces/SpaceCard/SpaceCard.js
+++ components/EventSpaces/SpaceCard/SpaceCard.js
@@ -3,45 +3,21 @@ import Link from "next/link";
 import Button from "../../Shared/Button/Button";
 import FeatureAvailability from "../../Shared/MembershipCard/MembershipSVGs/FeatureAvailability";
 import ShowMoreSvg from "./ShowMoreSvg";
-const SpaceCard = ({ image, title, size, seating, description, amenities, direction = "right", cardId }) => {
-  // State value to show or hide event spaced card
-  const [showCard, setShowCard] = useState(null);
-  // State value to hold the value of the current screen size
-  const [screenWidth, setScreenWidth] = useState(null);
-  const ref = useRef(null);
-  const handleVisibility = () => {
-    // const openCards = document.querySelector(`.spaces-card_open`);
-    // if (openCards !== null) {
-    //   openCards.className = openCards.className.replace("spaces-card_open", "spaces-card_closed");
-    // }
-    setShowCard(!showCard);
-  };
-  // keep the cards open on mobile screens
-  const showCardOnMobile = (screenWidth) => {
-    // for mobile screens less than 700px width
-    if (screenWidth < 700 && screenWidth !== null) {
-      setShowCard(true);
-    } else {
-      setShowCard(false);
-    }
-  };
-
-  // Get screen size
-  useEffect(() => {
-    // Set the screen size to screen width state on change
-    const onWidthChange = () => {
-      setScreenWidth(window.innerWidth);
-    };
-    showCardOnMobile(screenWidth);
-    // Add event listener to listen to screen width changes
-    window.addEventListener("resize", onWidthChange);
-    return () => window.removeEventListener("resize", onWidthChange); // removes the event listener
-  }, [screenWidth]);
-
+const SpaceCard = ({
+  image,
+  title,
+  size,
+  seating,
+  description,
+  amenities,
+  direction = "right",
+  cardVisibility,
+  handleVisibility,
+}) => {
   return (
     <div className={`spaces-card_container spaces-card_container_${direction}`}>
       <h2 className="spaces-card_container_title">{title}</h2>
-      <div className={`spaces-card spaces-card_${showCard ? "open" : "closed"}`} id={`card-${cardId}`} ref={ref}>
+      <div className={`spaces-card spaces-card_${cardVisibility ? "open" : "closed"}`}>
         <img src={`/images/spaces/${image ? image : ""}`} alt="spaces" />
         <div className="spaces-card_bg-gradient"></div>
         <div className="spaces-card_details">
@@ -81,7 +57,7 @@ const SpaceCard = ({ image, title, size, seating, description, amenities, direct
             handleVisibility();
           }}
         >
-          Show {showCard ? "Less" : "More"} <ShowMoreSvg className={`spaces-card_show-more_${direction}`} />
+          Show {cardVisibility ? "Less" : "More"} <ShowMoreSvg className={`spaces-card_show-more_${direction}`} />
         </button>
       </div>
     </div>
