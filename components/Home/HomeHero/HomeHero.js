a47db42d7e422d0cf490ed4c31e8102d0509e1d3
raihan <rishaon0@gmail.com>
2022-04-20 12:48:16 +0600

    ihd team's picture changed, new image added in the gallery

--- components/Home/HomeHero/HomeHero.js
+++ components/Home/HomeHero/HomeHero.js
@@ -6,8 +6,12 @@ const HomeHero = () => {
     <section className="home_hero">
       <video src="/video/hero_video.mp4" type="video/mp4" autoPlay muted loop></video>
       <div className="home_hero_book">
-        <Link href='/co-working/membership'>
-          <a><button>Book <br/> Now!</button></a>
+        <Link href="/coworking/membership">
+          <a>
+            <button>
+              Book <br /> Now!
+            </button>
+          </a>
         </Link>
       </div>
     </section>
