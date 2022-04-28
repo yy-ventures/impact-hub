0dbcedae42381e995f43585214ffc1816e34414a
raihan <rishaon0@gmail.com>
2022-04-28 12:33:36 +0600

    content data changed in about us team section

--- components/About_Us/AboutUsTeam/contents.js
+++ components/About_Us/AboutUsTeam/contents.js
@@ -1,8 +1,3 @@
-import aboutUsGridImage1 from "../../../public/images/about-us/about-cardv5.png";
-import aboutUsGridImage2 from "../../../public/images/about-us/about-cardv5.png";
-import aboutUsGridImage3 from "../../../public/images/about-us/about-cardv5.png";
-import aboutUsGridImage4 from "../../../public/images/about-us/about-cardv5.png";
-
 export const team = {
   span: "Meet our ",
   heading: "Team",
@@ -10,10 +5,3 @@ export const team = {
     "We are a team of thinkers & doers, movers & shakers, storytellers & bridge builders, boundary pushers & translators – but above all, we are social innovation enthusiasts who create a home for radical ideas that create positive social change.",
   ],
 };
-
-export const images = [
-  aboutUsGridImage1.src,
-  aboutUsGridImage2.src,
-  aboutUsGridImage3.src,
-  aboutUsGridImage4.src,
-];
